aec9f3a1ee67012019b63e8a7b381069235b7923
kurniawanpratama1999 <kurama.career@gmail.com>
2025-12-13 00:31:53 +0700

    UPDATE: perbaikan seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,49 @@
 import { prisma } from "../src/lib/prisma";
 
 async function main() {
-  await prisma.user.create({
-    data: {
-      name: "admin",
-      email: "admin@example.com",
-      password: "123456",
-    },
+  const transaction = await prisma.$transaction(async (trx) => {
+    const role = await trx.role.createMany({
+      data: [{ name: "Admin" }],
+    });
+
+    const anchor = await trx.anchor.createMany({
+      data: [
+        { icon: "", name: "user", url: "/user" },
+        { icon: "", name: "dashboard", url: "/dashboard" },
+        { icon: "", name: "order", url: "/order" },
+      ],
+    });
+
+    const user = await trx.user.create({
+      data: {
+        name: "admin",
+        roleId: 1,
+        email: "admin@mail.com",
+        password: "123456",
+      },
+    });
+
+    const roleAnchor = await trx.roleAnchor.createMany({
+      data: [
+        {
+          roleId: 1,
+          anchorId: 1,
+          merge: "1-1",
+        },
+        {
+          roleId: 1,
+          anchorId: 2,
+          merge: "1-2",
+        },
+        {
+          roleId: 1,
+          anchorId: 3,
+          merge: "1-3",
+        },
+      ],
+    });
+
+    return user;
   });
 }
 
--- src/app.ts
+++ src/app.ts
@@ -7,29 +7,59 @@ import rateLimit from "express-rate-limit";
 import { AuthRoute } from "./modules/routes/AuthRoutes";
 import { RoleRoute } from "./modules/routes/RoleRoutes";
 import path from "path";
+import { RoleMiddleware } from "./modules/middlewares/RoleMiddleware";
 
+// VERSIONING -> usage -> versioning("v1", "user");
+const versioning = (version: string, name: string): string =>
+  `/api/${version}/${name}`;
+
+// MIDDLEWARES -> usage -> ...protectedMiddlewares
+const protectedMiddlewares = (isActive: boolean = true) => {
+  if (!isActive) {
+    return [];
+  }
+
+  return [AuthMiddleware.handle, RoleMiddleware.handle];
+};
+
+// CORS -> usage -> app.use(corsConfig)
+const corsConfig = cors({
+  credentials: true,
+  methods: ["GET", "PUT", "PATCH", "DELETE", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-current-url"],
+  origin: ["http://localhost:5173", "https://demo-pos-express.vercel.app"],
+});
+
+// ACTIVATED MIDDLEWARE
+const middlewares = protectedMiddlewares(false);
+
+// INITIATE EXPRESS
 const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-    methods: ["GET", "PUT", "PATCH", "DELETE", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization", "x-current-url"],
-    origin: ["http://localhost:5173", "https://demo-pos-express.vercel.app"],
-  })
-);
+// CORS
+app.use(corsConfig);
+
+// HELMET
 app.use(helmet());
+
+// ALWAYS PARSE from JSON.stringify to JSON.parse
 app.use(express.json());
+
+// USER REQUEST LIMIT
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 
-// Routes
-app.get("/api/v1/documentation", (req: Request, res: Response) => {
-  const filePath = path.join(__dirname, "public", "documentation.html");
-  console.log(filePath);
-  return res.sendFile(filePath);
-});
-app.use("/api/v1/auth", AuthRoute);
-app.use("/api/v1/user", AuthMiddleware.handle, UserRoute);
-app.use("/api/v1/role", AuthMiddleware.handle, RoleRoute);
+// DOCUMENTATION
+app.get(versioning("v1", "documentation"), (req: Request, res: Response) =>
+  res.sendFile(path.join(__dirname, "public", "documentation.html"))
+);
+
+// AUTH
+app.use(versioning("v1", "auth"), AuthRoute);
+
+// USER
+app.use(versioning("v1", "user"), ...middlewares, UserRoute);
+
+// ROLE
+app.use(versioning("v1", "role"), ...middlewares, RoleRoute);
 
 export default app;
